e2444031f4d2215f14493cd5f6cb169565003dc1
Tommaso <lunlun@DESKTOP-O4RIU88.>
2024-10-28 11:49:34 +0100

    esercizio 1 esercitazione 2

---
+++ E02/Es01.py
@@ -0,0 +1,15 @@
+#esercizio 1 esercitazione 2
+# somma dei primi 100 numero naturali
+
+# si calcoli la somma dei primi 100 numeri naturali
+# (possibilmente usando un ciclo for)
+# si stampi il risultato della somma sullo schermo
+
+risultato = 0
+
+for i in range(1, 101): # l'estremo maggiore non è incluso nel range
+    risultato = risultato + i
+
+
+print(risultato)
+print(risultato)
